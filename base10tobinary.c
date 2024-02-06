f193633d48415802bbb6a1f06265f309171dc9a6
catnotadog <84086733+catnotadog@users.noreply.github.com>
2024-02-06 17:29:44 +0000

    Tue, Feb 6, 2024, 12:29 PM -05:00

--- base10tobinary.c
+++ base10tobinary.c
@@ -5,9 +5,12 @@
 int main(){
 int binary[8] = {0,0,0,0,0,0,0,0};
 int multiples[8] = {128,64,32,16,8,4,2,1};
-int num = 182;
+int num = 0;
+printf("enter base 10 number (0-255): ");
+scanf("%d",&num);
+if(num > 255){num = 255;} else if(num < 0){num = 0;}
 for(int i = 0; i < 8; i++){
-    if((num-multiples[i]) > 0){
+    if((num-multiples[i]) >= 0){
         binary[i] = 1;
         num -= multiples[i];
         printf("%d \n",num);
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # Base 10 to binary
+
+Convert Base 10 to binary (wow really???)
+
+todo:
+
+~~-take base 10 from input~~ 
